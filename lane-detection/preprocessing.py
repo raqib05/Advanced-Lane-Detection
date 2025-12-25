ed8e46d0467488669cc413ae5b949ea8d88d37d3
Ispahan Raqib <ispahanraqib2005@gmail.com>
2025-12-25 11:34:13 +0600

    edge detection setup

--- lane-detection/preprocessing.py
+++ lane-detection/preprocessing.py
@@ -5,10 +5,10 @@ def perspective_warp(img_path):
     img = cv2.imread(img_path)
     img = cv2.resize(img, (960, 540))
     src = np.float32([
-        (0.43, 0.67),  # top-left
-        (0.15, 0.95),  # bottom-left
-        (0.62, 0.67),  # top-right
-        (0.95, 0.95)   # bottom-right
+        (0.41, 0.65),  # top-left
+        (0.19, 0.9),  # bottom-left
+        (0.62, 0.65),  # top-right
+        (0.95, 0.9)   # bottom-right
     ])
     img_size = np.float32([(img.shape[1],img.shape[0])])
     src = src*img_size
@@ -24,10 +24,22 @@ def perspective_warp(img_path):
     ])
     t_matrix = cv2.getPerspectiveTransform(src, dst)
     birds_eye = cv2.warpPerspective(img, t_matrix, img_size)
+    cv2.imwrite("../data/birds_eye.png", birds_eye)
+    # cv2.imwrite("../data/coords.png", img)
     # cv2.imshow("test", img)
     # cv2.imshow("birds eye", birds_eye)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
     return birds_eye
+
+def edge_detection(img_path):
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    edges = cv2.Canny(img, 30, 60)
+    # cv2.imshow("birds eye", edges)
+    # cv2.waitKey(0)
+
+
+# perspective_warp("../data/lane5.png")
+# edge_detection("../data/birds_eye.png")
 
 # perspective_warp("../data/lane3.png")
